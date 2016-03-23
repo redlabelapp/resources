6423e9ebbc4bf7a7e55b2a1aef2b20a7238d2948
brito <francisco.brito@oracle.com>
2016-03-23 16:55:40 -0700

    Reformat

--- resources.js
+++ resources.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 /**
 Generate icon and splash screen resources
 
@@ -6,71 +7,70 @@ Takes an image 'icon.png' and outputs to pre-existing directories res/*
 
 Requires Imagemagick (imagemagick.org)
 */
-(function(specification){
-	var shell = require('child_process');
-	for (var location in specification)
-		for (var namesize in specification[location])
-			(location + specification[location][namesize])
-				.replace(/(^\S+) (\d+)x(\d+)?/,
-					function(spec, output, width, height){
-						var command = '/opt/local/bin/convert icon.png ' +
-							(width == height ? 
-									'-resize ' + width :
-									'-gravity center -resize ' + Math.floor((Math.min(width, height))/5.3) + ' -extent ' + width + 'x' + height)
-							+ ' ' + output;
-						console.log(command);
-						shell.exec(command, function(error, stdout, stderr){ 
-							console.log(stdout);
-							console.log(stderr);
-							if (error)
-								console.log(error);
-						});
-					});
+(function(specification) {
+    var shell = require('child_process');
+    for (var location in specification)
+        for (var namesize in specification[location])
+            (location + specification[location][namesize])
+            .replace(/(^\S+) (\d+)x(\d+)?/,
+                function(spec, output, width, height) {
+                    var command = '/opt/local/bin/convert icon.png ' +
+                        (width == height ?
+                            '-resize ' + width :
+                            '-gravity center -resize ' + Math.floor((Math.min(width, height)) / 5.333) + ' -extent ' + width + 'x' + height) + ' ' + output;
+                    console.log(command);
+                    shell.exec(command, function(error, stdout, stderr) {
+                        console.log(stdout);
+                        console.log(stderr);
+                        if (error)
+                            console.log(error);
+                    });
+                });
 })({
-	'res/ios/': [ 
-		'iTunesArtwork.png 512x512',
+    'res/ios/': [
+        'iTunesArtwork.png 512x512',
 
-		'Icon-120.png 120x120',
-		'Icon-60@3x.png 180x180',
-		'Icon-76.png 76x76',
-		'Icon-76@2x.png 152x152',
+        'Icon-120.png 120x120',
+        'Icon-60@3x.png 180x180',
+        'Icon-76.png 76x76',
+        'Icon-76@2x.png 152x152',
 
-		'Default@2x.png 640x960',
-		'Default-568h@2x.png 640x1136',
-		'Default-667h@2x.png 750x1334',
-		'Default-1104h@2x.png 1242x2208',
-		'Default-Landscape-621@2x.png 2208x1242',
-		'Default-PortraitRetina.png 768x1024',
-		'Default-PortraitRetina@2x.png 1536x2048',
-		'Default-LandscapeRetina.png 1024x768', 
-		'Default-LandscapeRetina@2x.png 2048x1536',
+        'Default@2x.png 640x960',
+        'Default-568h@2x.png 640x1136',
+        'Default-667h@2x.png 750x1334',
+        'Default-1104h@2x.png 1242x2208',
+        'Default-Landscape-621@2x.png 2208x1242',
+        'Default-PortraitRetina.png 768x1024',
+        'Default-PortraitRetina@2x.png 1536x2048',
+        'Default-LandscapeRetina.png 1024x768',
+        'Default-LandscapeRetina@2x.png 2048x1536',
 
-		'Icon-40.png 40x40', 
-		'Icon-40@2x.png 80x80', 
-		'Icon-40@3x.png 120x120',
+        'Icon-40.png 40x40',
+        'Icon-40@2x.png 80x80',
+        'Icon-40@3x.png 120x120',
 
-		'Icon-29.png 29x29',
-		'Icon-29@2x.png 80x80',
-		'Icon-29@3x.png 87x87'
-	],
+        'Icon-29.png 29x29',
+        'Icon-29@2x.png 80x80',
+        'Icon-29@3x.png 87x87'
+    ],
 
-	'res/android/': [
-		'display-ldpi-icon.png 36x36',
-		'display-port-ldpi-splashscreen.png 480x800',
-		'display-land-ldpi-splashscreen.png 800x480',
+    'res/android/': [
+        'display-ldpi-icon.png 36x36',
+        'display-port-ldpi-splashscreen.png 480x800',
+        'display-land-ldpi-splashscreen.png 800x480',
 
-		'display-mdpi-icon.png 48x48',
-		'display-port-mdpi-splashscreen.png 480x800',
-		'display-land-mdpi-splashscreen.png 800x480',
+        'display-mdpi-icon.png 48x48',
+        'display-port-mdpi-splashscreen.png 480x800',
+        'display-land-mdpi-splashscreen.png 800x480',
 
-		'display-hdpi-icon.png 72x72',
-		'display-port-hdpi-splashscreen.png 800x1280',
-		'display-land-hdpi-splashscreen.png 1280x800',
+        'display-hdpi-icon.png 72x72',
+        'display-port-hdpi-splashscreen.png 800x1280',
+        'display-land-hdpi-splashscreen.png 1280x800',
 
-		'display-xhdpi-icon.png 96x96',
-		'display-port-xhdpi-splashscreen.png 768x1024',
-		'display-land-xhdpi-splashscreen.png 1024x768'
-	]
+        'display-xhdpi-icon.png 96x96',
+        'display-port-xhdpi-splashscreen.png 768x1024',
+        'display-land-xhdpi-splashscreen.png 1024x768'
+    ]
 });
 
 /*
@@ -79,4 +79,3 @@ Requires Imagemagick (imagemagick.org)
 			"icons for universal apps" site:developer.apple.com
 		https://developer.apple.com/library/ios/qa/qa1686/_index.html
 */
-
